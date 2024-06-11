19cff070724c5f2417177596db87c1d4bc3b50d6
Sedly <sedly.keith@student.moringaschool.com>
2024-06-11 11:15:25 +0300

    moringa-FT09-phase-3-code-challenge

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,29 +4,35 @@ from models.article import Article
 from models.magazine import Magazine
 
 class TestModels(unittest.TestCase):
-    def test_author_creation_valid(self):
-        """Test Author creation with valid data."""
+    def test_author_creation(self):
         author = Author(1, "John Doe")
         self.assertEqual(author.name, "John Doe")
 
-    def test_article_creation_valid(self):
-        """Test Article creation with valid data."""
-        # Providing longer content for testing
-        article = Article(1, "Test Title", "A" * 50, 1, 1)
+    def test_article_creation(self):
+        article = Article(1, "Test Title", "Test Content", 1, 1)
         self.assertEqual(article.title, "Test Title")
 
-    def test_magazine_creation_valid(self):
-        """Test Magazine creation with valid data."""
-        # Providing both name and category for Magazine creation
-        magazine = Magazine(1, "Tech Weekly", "Technology")
+    def test_magazine_creation(self):
+        magazine = Magazine(1, "Tech Weekly")
         self.assertEqual(magazine.name, "Tech Weekly")
-        self.assertEqual(magazine.category, "Technology")
+        
+    def test_list_all_magazines(self):
+        all_magazine = Magazine.list_all_magazines()
+        self.assertGreaterEqual(len(all_magazine),1)
 
-    def test_magazine_creation_invalid(self):
-        """Test Magazine creation with invalid data."""
-        with self.assertRaises(ValueError):
-            # Providing empty name and missing category for Magazine creation
-            magazine = Magazine(1, "", "")  
+    def test_list_all_authors(self):
+        all_author = Author.list_all_authors()
+        self.assertGreaterEqual(len(all_author),1)
+
+    def test_list_all_articles(self):
+        all_article = Article.list_all_articles()
+        self.assertGreaterEqual(len(all_article),1)
+
+    def test_magazine_contributing_authors(self):
+        magazine = Magazine(1, "Vogue", "Fashion")
+        authors = magazine.contributing_authors
+        self.assertIsInstance(authors, list)
+        self.assertTrue(all(isinstance(author, Author) for author in authors))
 
 if __name__ == "__main__":
     unittest.main()
